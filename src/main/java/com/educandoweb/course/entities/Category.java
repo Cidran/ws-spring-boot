066d8e9bf0cc927705819b59d9d8e32d565e1576
Phillipe Cidran <phillipe.cidrao@gmail.com>
2026-04-26 08:05:30 -0300

    product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,7 +1,9 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,6 +21,8 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	private Set<Product> products = new HashSet<Product>();
 
 	public Category() {
 
@@ -49,6 +53,10 @@ public class Category implements Serializable {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
